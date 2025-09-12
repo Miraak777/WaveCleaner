2f2b8abf105e6c2d092b4ea808cc456f8ce8f26f
Miraak777 <miraakbs@gmail.com>
2025-09-12 14:53:44 +0300

    feat: added player taking damage mechanic

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,8 +1,20 @@
 extends CharacterBody2D
 
-@export var move_speed: float = 200
+@export var base_move_speed: float = 200
+@export var base_max_health: float = 100
+
+var move_speed: float
+var max_health: float
+var health: float
 
 var is_moving: bool = false
+
+signal player_died
+
+func _ready() -> void:
+	move_speed = base_move_speed
+	max_health = base_max_health
+	health = max_health
 
 func _process(delta: float) -> void:
 	handle_idle_animation()
@@ -28,7 +40,6 @@ func handle_move_animation() -> void:
 func _physics_process(delta: float) -> void:
 	handle_move()
 
-
 func handle_move() -> void:
 	velocity = Vector2.ZERO
 	var direction = Input.get_vector("MoveLeft", "MoveRight", "MoveUp", "MoveDown")
@@ -38,3 +49,9 @@ func handle_move() -> void:
 		is_moving = false
 	velocity = direction * move_speed
 	move_and_slide()
+
+func take_damage(damage) -> void:
+	health -= damage
+	print(health)
+	if health < 0:
+		player_died.emit()
